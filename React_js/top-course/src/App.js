bf02908f9c91d59e8dd826fb966b5cfd8d920f1f
Anshul Vishwakarma <72655193+AnsSec@users.noreply.github.com>
2023-04-20 18:25:17 +0530

    .

--- React_js/top-course/src/App.js
+++ React_js/top-course/src/App.js
@@ -4,25 +4,44 @@ import Filter from "./component/Filter";
 import Cards from "./component/Cards";
 import { filterData, apiUrl } from "./data";
 import { toast } from "react-toastify";
-const App = () => {
-  // const [courses,setCourses]=useState()
-  useEffect(() => {
-    const fetchdata = async () => {
-      fetch(apiUrl)
-        .then((data) => data.json())
-        .then((res) => {
-          console.log(res);
-        })
-        .catch((error) => toast.error("somthing is wrong"));
-    };
-    fetchdata();
-  }, []);
+import Spinner from "./component/Spinner";
 
+
+const App = () => {
+  const [courses, setCourses] = useState(null);
+  const [loading,setLoading]=useState(true)
+
+  const fetchdata = async () => {
+    setLoading(true)
+    fetch(apiUrl)
+      .then((data) => data.json())
+      .then((res) => {
+        setCourses(res.data);
+      })
+      .catch((error) => {
+        toast.error(`somthing is wrong`);
+        console.log(error);
+      });
+      setLoading(false)
+  };
+  
+
+  useEffect(()=>{
+    fetchdata();
+  },[])
   return (
     <div>
+      <div>
       <NavBar />
+      </div>
+      <div>
       <Filter filterData={filterData} />
-      <Cards />
+      </div>
+      <div>
+      {
+        loading ? (<Spinner/>):(<Cards/>)
+      }
+      </div>
     </div>
   );
 };
--- React_js/top-course/src/component/Cards.jsx
+++ React_js/top-course/src/component/Cards.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Card from './Card'
 
-const Cards = () => {
-  return (
+const Cards = ({courses}) => {
+    let allCourses=[];
+
+
+    const getCourses=()=>{
+        Object.values(courses).forEach((courseCatogery)=>{
+            courseCatogery.forEach((course)=>{
+                allCourses.push(course)
+            })
+        })
+        return allCourses;
+    }
+
+    return (
     <div>
-        
+        {
+            getCourses().map( (course)=>{
+                return <Card course={course}/>
+            })
+        }
     </div>
   )
 }
