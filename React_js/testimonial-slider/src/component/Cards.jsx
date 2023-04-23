c17e83eb1c3921f8b54dcc298007f206ef593cb0
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-04-23 18:15:56 +0530

    ..

--- React_js/testimonial-slider/src/component/Cards.jsx
+++ React_js/testimonial-slider/src/component/Cards.jsx
@@ -1,36 +1,41 @@
 import React from 'react'
-import {FaQuoteLeft,FaQuoteRight,FaGem} from "react-icons/fa"
+import {FaQuoteLeft,FaQuoteRight} from "react-icons/fa";
+import {FiChevronLeft,FiChevronRight} from "react-icons/fi"
 const Cards = ({review}) => {
   return (
     <div className='flex flex-col md:relative'>
-        <div className='absolute -top-[7rem] z-[10] mx-auto' >
-            <img className='aspect-square rounded-full w-[140px] h-[140px] z-25' src={review.image} alt="" />
+        <div className='absolute top-[-7rem] z-[10] mx-auto' >
+            
+            <img className='aspect-square rounded-full w-[140px] h-[140px] z-[25]' src={review.image} alt="" />
+            <div className='w-[140px] h-[140px] bg-violet-500 rounded-full absolute top-[-6px] z-[-10] 
+            left-[10px]'></div>
         </div>
-        <div>
-            <p>{review.name}</p>
+        
+        <div className='text-center mt-7'>
+            <p className='font-bold text-2xl capitalize'>{review.name}</p>
         </div>
 
-        <div>
-            <p>{review.job}</p>
+        <div className='text-center mt-7'>
+            <p className='text-violet-300 uppercase text-sm'>{review.job}</p>
         </div>
-        <div>
+        <div className='text-violet-400 mx-auto mt-5 '>
             <FaQuoteLeft/>
         </div>
 
-        <div>
+        <div className='text-center mt-4 text-slate-500'>
             <p>{review.text}</p>
         </div>
 
-        <div>
+        <div className='text-violet-400 mx-auto mt-5 '>
             <FaQuoteRight/>
         </div>
 
-        <div>
-            <button>
-                +
+        <div className='flex text-3xl text-violet-400 mt-5 gap-3 font-bold'>
+            <button className='hover:text-violet-500'>
+                <FiChevronLeft/>
             </button>
-            <button>
-                *
+            <button className='hover:text-violet-500'>
+                <FiChevronRight/>
             </button>
         </div>
 
