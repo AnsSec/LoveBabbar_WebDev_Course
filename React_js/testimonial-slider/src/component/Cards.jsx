f350d947f1c170cf0957b05b8207716c41ab4070
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-04-23 19:14:49 +0530

    ..

--- React_js/testimonial-slider/src/component/Cards.jsx
+++ React_js/testimonial-slider/src/component/Cards.jsx
@@ -12,12 +12,10 @@ const Cards = ({review}) => {
         </div>
         
         <div className='text-center mt-7'>
-            <p className='font-bold text-2xl capitalize'>{review.name}</p>
-        </div>
-
-        <div className='text-center mt-7'>
+            <p className='font-bold text-2xl tracking-wide  capitalize'>{review.name}</p>
             <p className='text-violet-300 uppercase text-sm'>{review.job}</p>
         </div>
+
         <div className='text-violet-400 mx-auto mt-5 '>
             <FaQuoteLeft/>
         </div>
--- React_js/testimonial-slider/src/component/Testimonial.jsx
+++ React_js/testimonial-slider/src/component/Testimonial.jsx
@@ -1,26 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Cards from './Cards';
 import {FiChevronLeft,FiChevronRight} from "react-icons/fi";
 
 
 const Testimonial = ({reviews}) => {
-  
+  let [index,setIndex]=useState(0);
   const leftShiftHandler=()=>{
-
+    if(index-1<0){
+      setIndex(reviews.length-1);
+    }else{
+      setIndex(index-1)
+    }
   }
 
   const rightShiftHandler=()=>{
-
+    if(index+1>=reviews.length){
+      setIndex(0);
+    }else{
+      setIndex(index+1);
+    }
   }
 
   const surpriseHandler=()=>{
-    
+    let randomIndex = Math.floor(Math.random()*reviews.length);
+    setIndex(randomIndex)
   }
 
 
   return (
-    <div>
-        <Cards review={reviews[0]}></Cards>
+    <div className='w-[85vw] md:w-[700px] bg-white flex flex-col justify-center items-center mt-10 p-10 transition-all duration-700 hover:shadow-xl rounded-md'>
+        <Cards review={reviews[index]}></Cards>
         <div className='flex text-3xl text-violet-400 my-5 gap-3 font-bold justify-center'>
             <button onClick={leftShiftHandler} className='hover:text-violet-500'>
                 <FiChevronLeft/>
