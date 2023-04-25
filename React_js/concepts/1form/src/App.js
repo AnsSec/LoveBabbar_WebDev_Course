5cfb44d4032abbb495bf5f577f0107fdebd6ab6e
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-04-25 15:05:51 +0530

    .

---
+++ React_js/concepts/1form/src/App.js
@@ -0,0 +1,83 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+
+  // const [firstName, setFirstName] = useState("");
+  // const [lastName, setLastName] = useState("");
+
+  // console.log(firstName);
+  // console.log(lastName);
+  // function changeFirstNameHandler(event) {
+  //   // console.log("printing first name")
+  //   // console.log(event.target.value);
+  //   setFirstName(event.target.value);
+  // }
+  // function changelastNameHandler(event) {
+  //   // console.log("Printing last name")
+  //   // console.log(event.target.value);
+  //   setLastName(event.target.value);
+  // }
+
+  const [formData, setFormData] = useState(  {firstName: "", 
+  lastName: "", email:"" } );
+
+  // console.log(formData)
+
+  function changeHandler(event) {
+    const {name, value, checked, type} = event.target
+    setFormData(prevFormData => {
+      return {
+        ...prevFormData,
+        [name]: type ==="checkbox" ? checked : value
+      }
+    });
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+    //print
+    console.log("Finally printing the entireform ka data ........")
+    console.log(formData)
+  }
+
+  return (
+    <div className="App">
+      <form onSubmit={submitHandler}>
+      <br/>
+        <input
+          type="text"
+          placeholder='first name'
+          onChange={changeHandler}  
+          name="firstName"
+          value={formData.firstName}
+        />
+
+        <br/>
+        <br></br>
+
+        <input
+          type="text"
+          placeholder='last name'
+          onChange={changeHandler} 
+          name="lastName" 
+          
+        />
+
+        <br/>
+        <br/>
+        <input
+          type="email"
+          placeholder="Enter your email here"
+          onChange={changeHandler}
+          name="email"
+            
+          />
+
+      </form>
+    </div>
+  );
+}
+
+export default App;
