3e1e56838793208164aba43716eeae357999574c
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-04-29 16:15:38 +0530

    ..

--- React_js/random-gif-starter/src/App.jsx
+++ React_js/random-gif-starter/src/App.jsx
@@ -1,3 +1,17 @@
-export default function App() {
-  return <div>Random GIF</div>;
-}
+import React from "react";
+import Random from "./components/Random";
+import Tags from "./components/Tags";
+
+const App = () => {
+  return (
+    <div className="w-full h-screen flex flex-col background  overflow-x-hidden items-center">
+      <h1 className="bg-white rounded-xl  w-11/12 mt-10 text-center mx-6 px-10 py-2 text-3xl font-bold uppercase">Random GIF</h1>
+      <div className="flex flex-col w-full items-center gap-y-10 mt-8">
+        <Random />
+        <Tags />
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- React_js/random-gif-starter/src/components/Random.jsx
+++ React_js/random-gif-starter/src/components/Random.jsx
@@ -1,3 +1,21 @@
-export default function Random() {
-  return <div>Random</div>;
+import React, { useState } from 'react'
+
+const Random = () => {
+
+  const [gif,setGif]=useState('');
+     
+  const clickHandler=()=>{
+
+  }
+  return (
+    <div className='w-1/2 h-64 bg-green-400 rounded-2xl border  border-black flex flex-col items-center gap-y-5 mt-4'>
+        <h1 className='text-2xl uppercase underline mt-4 font-bold'>Random Gif</h1>
+        <img src={gif} alt="" width={450}/>
+        <button onClick={clickHandler}>
+            Generate
+        </button>
+    </div>
+  )
 }
+
+export default Random
