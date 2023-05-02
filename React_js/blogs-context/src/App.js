754747a037b8a012089656b223b60833adc5478b
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-05-02 20:03:10 +0530

    .

--- React_js/blogs-context/src/App.js
+++ React_js/blogs-context/src/App.js
@@ -2,13 +2,13 @@ import { useContext, useEffect } from "react";
 import Blog from "./Component/Blog";
 import Header from "./Component/Header";
 import Pagination from "./Component/Pagination";
-import { AppContext } from "./Context/AppContext";
+import AppContext  from "./Context/AppContext";
 
 export default function App() {
-  const {fetchBlogPost}=useContext(AppContext);
+  const {fetchBlogPost}=useContext(AppContext)
 
   useEffect(() => {
-    fetchBlogPost() 
+    fetchBlogPost();
   },[]);
 
   return (
