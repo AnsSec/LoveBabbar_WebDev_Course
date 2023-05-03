d1f963f3f7f35103d49cabafcdbf88e45ba0ebb9
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-05-03 21:41:56 +0530

    ..

--- React_js/blogs-context/src/App.js
+++ React_js/blogs-context/src/App.js
@@ -1,3 +1,4 @@
+import { Route, Routes } from "react-router-dom";
 import "../src/App.css"
 import Blog from "./Component/Blog";
 import Header from "./Component/Header";
@@ -14,11 +15,18 @@ export default function App() {
   },[]);
 
   return (
-    <div className="w-full h-full flex flex-col gap-y-1 justify-center items-center">
-      <Header/>
-      <Blog/>
-      <Pagination/>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/blog/:blogId" element={<BlogPage/>}/>
+      <Route path="/tags/:tag" element={<TagPage/>}/>
+      <Route path="/categories/:category" element={<CategoryPage/>}/>
+
+    </Routes>
+    // <div className="w-full h-full flex flex-col gap-y-1 justify-center items-center">
+    //   <Header/>
+    //   <Blog/>
+    //   <Pagination/>
+    // </div>
   ) ;
   
   
