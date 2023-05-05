737abd8d7e23019bba3ce1470bf4966b31174e37
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-05-05 10:57:28 +0530

    ..

--- React_js/shopping-cart-(redux)/src/redux/Slices/cartSlice.js
+++ React_js/shopping-cart-(redux)/src/redux/Slices/cartSlice.js
@@ -4,5 +4,11 @@ import { createSlice } from "@reduxjs/toolkit";
 export const CartSlice=createSlice({
     name:"cart",
     initialState:[],
-    reducers
-})
+    reducers:{
+        add:()=>{},
+        remove:()=>{},
+    }
+});
+
+export const {add,remove}=CartSlice.actions;
+export default CartSlice.reducer;
