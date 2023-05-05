d54099c83e2223120a01b9c6f4331298aba6117b
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-05-05 15:55:44 +0530

    ..

--- React_js/shopping-cart-(redux)/src/components/Navbar.jsx
+++ React_js/shopping-cart-(redux)/src/components/Navbar.jsx
@@ -1,5 +1,28 @@
+import { FaShoppingCart } from "react-icons/fa";
+import logo from "./Daco_5715743.png";
+import { NavLink } from "react-router-dom";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <div className="flex flex-row justify-between">
+        <NavLink to="/">
+          <div>
+            <img src={logo} alt="" />
+          </div>
+        </NavLink>
+      </div>
+      <div>
+        <NavLink to="/">
+          <p>Home</p>
+        </NavLink>
+        <NavLink to="/">
+          <div>
+            <FaShoppingCart />
+          </div>
+        </NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- React_js/shopping-cart-(redux)/src/pages/Home.jsx
+++ React_js/shopping-cart-(redux)/src/pages/Home.jsx
@@ -1,7 +1,50 @@
+import { useEffect, useState } from "react";
+import Spinner from '../components/Spinner'
+
 const Home = () => {
   const API_URL = "https://fakestoreapi.com/products";
+  const [loading,setLoading]=useState(false);
+  const [post,setPost]=useState([]);
 
-  return <div>Home</div>;
+  const fetchProductData=async=()=>{
+    setLoading(true);
+    fetch(API_URL)
+    .then(res=>res.json())
+    .then(data=>{
+      setPost(data)
+    })
+
+    .catch(error=>{
+      console.log("Error...")
+      setPost([]);
+    })
+
+    setLoading(false);
+  }
+
+  useEffect(()=>{
+    fetchProductData();
+  },[]);
+
+
+  return (
+    <div>
+      {
+        loading?<Spinner/>:
+        post.length>0?
+        (<div>
+          {
+            post.map((post)=>(
+              <Product key={post.id} post={post}/>
+            ))
+          }
+        </div>):
+        <div>
+          <p>No Data found</p>
+        </div>
+      }
+    </div>
+  );
 };
 
 export default Home;
