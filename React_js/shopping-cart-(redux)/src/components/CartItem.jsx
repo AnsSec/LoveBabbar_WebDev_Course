0aa720e806ff8ab62397ecaf7c883f4dd7a1bd58
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-05-07 14:02:26 +0530

    ..

--- React_js/shopping-cart-(redux)/src/components/CartItem.jsx
+++ React_js/shopping-cart-(redux)/src/components/CartItem.jsx
@@ -1,7 +1,21 @@
-const CartItem = () => {
+import {FcDeleteDatabase} from "react-icons/fc"
+
+const CartItem = ({item,itemIndex}) => {
   return (
     <div>
-      
+      <div>
+        <div>
+          <img src={item.image} />
+        </div>
+        <div>
+          <h1>{item.title}</h1>
+          <h1>{item.description}</h1>
+          <div>
+            <p>{item.price}</p>
+            <FcDeleteDatabase/>
+          </div>
+        </div>
+      </div>
     </div>
   )
 };
