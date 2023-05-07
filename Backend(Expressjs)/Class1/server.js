9f0043a05e8067cd7966fb08be2bc701570af36a
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-05-07 20:41:41 +0530

    ..

--- Backend(Expressjs)/Class1/server.js
+++ Backend(Expressjs)/Class1/server.js
@@ -1,12 +1,27 @@
 const express=require("express");
 const app=express();
 
+//used to parse req.body in express -> PUT or POST
+const bodyParser=require("body-parser");
+
+//specifically parse JSON data & add it to the request.Body object
+app.use(bodyParser.json());
+
+
 const port=4000
 
 app.listen(port,()=>{
     console.log(`App Started port number ${port}`)
 })
 
-app.get('/',(req,res)=>{
+//routs
+app.get('/',(req,res)=>{  //data server se lena 
     res.send("hello")
+})
+
+app.post("/api/cars",(req,res)=>{ //data server ko bhejna 
+    const {name,brand}=req.body;
+    console.log(name);
+    console.log(brand);
+    res.send("car send successfully.")
 })
