2d929aa2c79037deb5a24a6d41f3daac2a53261a
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-05-07 11:35:58 +0530

    .

--- React_js/shopping-cart-(redux)/src/pages/Cart.jsx
+++ React_js/shopping-cart-(redux)/src/pages/Cart.jsx
@@ -1,5 +1,30 @@
+import { useSelector } from "react-redux";
+
 const Cart = () => {
-  return <div>Cart</div>;
+  const { cart } = useSelector((state) => state);
+
+  return (
+    <div>
+      {cart.length > 0 ? (
+        <div> 
+          <div>
+            {
+              cart.map((item,index)=>(
+                <CartItem key={item.id} item={item} itemIndex={index}/>
+              ))
+            }
+          </div>
+        </div>
+      ) : (
+        <div>
+          <h2>Cart Empty!</h2>
+          <Link to="/">
+            <button>Shop Now</button>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Cart;
