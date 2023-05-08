7233b03ed4173f33a82aae53a537189002eea986
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-05-08 14:19:53 +0530

    ..

---
+++ Backend(Expressjs)/Class2/Config/database.js
@@ -0,0 +1,16 @@
+const mongoose=require('mongoose')
+
+require('dotenv').config();
+
+const dbConnect=()=>{
+    mongoose.connect(process.env.db_url,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+    })
+    .then(()=>console.log('connection successfull'))
+    .catch(()=>{console.log('connection error');
+        process.exit(1);
+    });
+}
+
+module.exports=dbConnect;
---
+++ Backend(Expressjs)/Class2/Controllers/createTodo.js
@@ -0,0 +1,12 @@
+//import the schema module
+const todo=require('../Models/Todo')
+
+//define route handler
+exports.createTodo=async(req,res)=>{
+    try{
+        
+    }
+    catch(err){
+
+    }
+}
