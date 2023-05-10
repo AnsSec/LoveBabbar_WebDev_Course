ce16e7e6a9bb18cb8047aaa7b5c8e16772a59d43
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-05-10 13:57:34 +0530

    ..

--- Backend(Expressjs)/Blog App/index.js
+++ Backend(Expressjs)/Blog App/index.js
@@ -2,9 +2,18 @@ const express=require('express')
 const app=express();
 
 require('dotenv').config();
-
 const PORT=process.env.PORT||5000
 
+//middleware
+app.use(express.json());
+
+const blog=require('./route/blog')
+//mount
+app.use('/api/v1',blog);
+
 app.listen(PORT,()=>{
-    console.log("Server Started")
+    console.log(`Server Started ${PORT}`)
 })
+
+const connectwithdb=require('./config/database')
+connectwithdb();
