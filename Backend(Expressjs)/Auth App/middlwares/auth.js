1fbc545acb171d09671b7fc7c7cee8884f039363
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-05-13 09:57:08 +0530

    ..

--- Backend(Expressjs)/Auth App/middlwares/auth.js
+++ Backend(Expressjs)/Auth App/middlwares/auth.js
@@ -15,7 +15,56 @@ exports.auth=(req,res,next)=>{
             });
         };
         
+        //verify the token
+        try {
+            const decode=jwt.verify(token,process.env.JWT_SECRET);
+            console.log(decode);
+            req.user=decode;
+        } catch (error) {
+            return res.status(401).json({
+                success:false,
+                message:"token is invalid"
+            })
+        }
+        next();
     } catch (error) {
-        
+        return res.status(401).json({
+            success:false,
+            message:"Some thing went wrong"
+        })   
+    }
+}
+
+exports.isStudent=(req,res,next)=>{
+    try {
+        if (req.user.role!="Student") {
+            return res.status(401).json({
+                success:false,
+                message:"This is procted rout for student",
+            });
+        }
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            success:false,
+            message:"User Role is not matching",
+        });
+    }
+}
+
+exports.isAdmin=(req,res,next)=>{
+    try {
+        if (req.user.role!="Admin") {
+            return res.status(401).json({
+                success:false,
+                message:"This is procted rout for Admin",
+            });
+        }
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            success:false,
+            message:"User Role is not matching",
+        });
     }
 }
