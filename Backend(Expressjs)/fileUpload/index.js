4fb9b907f30ac6c590877c7010b3d7c7a380240a
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-05-15 11:43:21 +0530

    ..

---
+++ Backend(Expressjs)/fileUpload/index.js
@@ -0,0 +1,28 @@
+const express=require('express')
+const app=express();
+
+//port find
+require('dotenv').config();
+const PORT=process.env.PORT;
+
+//middleware
+app.use(express.json());
+const fileUpload=require('express-fileupload');
+app.use(fileUpload());
+
+//Database connection
+const db=require('./config/database');
+db.connect();
+
+//connection from cloud
+const cloudinary=require('./config/cloudinary');
+cloudinary.cloudinaryConnect();
+
+//api routs mount
+const Upload=require('./routes/fileUpload');
+app.use('api/v1/upload');
+
+//server start
+app.listen(PORT,()=>{
+    console.log(`server started at ${PORT}`)
+})
