a6bb7652e8d4738d34e8fa043c8543380bf82d11
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-05-18 15:27:27 +0530

    ..

--- Backend(Expressjs)/fileUpload/routes/fileUpload.js
+++ Backend(Expressjs)/fileUpload/routes/fileUpload.js
@@ -1,11 +1,12 @@
 const express=require('express');
 const router=express.Router();
 
-const {localfileUpload,imageUploader}=require('../controllers/fileUpload');
+const {localfileUpload,imageUploader,videoUploader}=require('../controllers/fileUpload');
 
 //api route
 router.post('/localFileUpload',localfileUpload);
 router.post('/imageUploader',imageUploader);
+router.post('/videoUploader',videoUploader);
 
 module.exports=router;
 
