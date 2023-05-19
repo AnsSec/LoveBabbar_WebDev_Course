ba4630cd4dd2c1d8020b90c65c6d6ad285043233
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-05-19 17:39:13 +0530

    .

--- Backend(Expressjs)/Blog App/index.js
+++ Backend(Expressjs)/Blog App/index.js
@@ -7,7 +7,7 @@ const PORT=process.env.PORT||5000
 //middleware
 app.use(express.json());
 
-const blog=require('./route/blog')
+const blog=require('./routes/blog')
 //mount
 app.use('/api/v1',blog);
 
